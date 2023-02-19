a64767fd3383def171bbfd43127e1288a4344630
sudip <89732129+sudiplun@users.noreply.github.com>
2023-02-19 21:29:33 +0545

    install
    script that install FireFox

---
+++ FireFox_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo"Downloading FireFox_Setup_File for Offcial sites"
+wget -O ~/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64"
+#extacting the latest firefox .tar.bz2 to /opt/
+sudo tar xjf ~/FirefoxSetup.tar.bz2 -C /opt/
+#creating symbolic link to system binary 
+sudo ln -s /opt/firefox/firefox /usr/bin/
+echo"To create an app icon on Menu create Firefox.desktop file on /usr/share/applications or ~/.local/share/applications"
+#removing download setup file
+echo"it's better to rm ~/FirefoxSetup.tar.bz2 sure.........!"
+rm -rf ~/FirefoxSetup.tar.bz2
