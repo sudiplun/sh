b2372ede370f678ff44e83be0649f3fa5079a2bd
sudip <89732129+sudiplun@users.noreply.github.com>
2023-02-20 20:34:12 +0545

    fixing menu

--- FireFox_install.sh
+++ FireFox_install.sh
@@ -5,7 +5,10 @@ wget -O ~/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-la
 sudo tar xjf ~/FirefoxSetup.tar.bz2 -C /opt/
 #creating symbolic link to system binary 
 sudo ln -s /opt/firefox/firefox /usr/bin/
-echo"To create an app icon on Menu create Firefox.desktop file on /usr/share/applications or ~/.local/share/applications"
+#echo"To create an app icon on Menu create Firefox.desktop file on /usr/share/applications or ~/.local/share/applications"
+#setup desktop icon
+sudo wget "https://raw.githubusercontent.com/sudiplun/.desktop/main/Firefox.desktop" -P /usr/local/share/applications
 #removing download setup file
 echo"it's better to rm ~/FirefoxSetup.tar.bz2 sure.........!"
 rm -rf ~/FirefoxSetup.tar.bz2
+#done
