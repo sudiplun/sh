5e7c4635c0b4f60c77649893590d4d9f6e571da0
sudiplun <sudeeplun@gmail.com>
2023-03-13 14:03:01 +0545

    dd

--- update.sh
+++ update.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-
-echo "WE ARE GOING TO UPGRADING APT && FLATPAK PACKAGE "
-
+echo "I am going to make your System upto-date including flatpak"
 #apt update
 sudo apt update
-
 #apt upgrading
 sudo apt upgrade -y
-
 #flatpak update
 flatpak update
-
 #autoclean
 sudo apt-get autoclean && sudo apt-get autoremove
-
 #done
 echo "Done apt && flatpak update"
