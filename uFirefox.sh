56e8b028e0321caa0d6b04661997b8879b3f4a99
sudiplun <sudeeplun@gmail.com>
2023-03-15 09:01:46 +0545

    fix

---
+++ uFirefox.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo"Downloading FireFox_Setup_File for Offcial sites"
+wget -O ~/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64"
+#extacting the latest firefox .tar.bz2 to /opt/
+sudo tar xjf ~/FirefoxSetup.tar.bz2 -C /opt/
+echo"it's better to rm ~/FirefoxSetup.tar.bz2 sure..... ( ͡❛ ͜ʖ ͡❛)....!"
+rm ~/FirefoxSetup.tar.bz2
